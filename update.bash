6e1a86e3c59bd76a856670d92f64fa1cb2034ca8
Grig Larson <GrigLars@users.noreply.github.com>
2020-07-19 15:52:17 -0400

    updated to root only, removed sudo, fixed quotes

--- update.bash
+++ update.bash
@@ -3,37 +3,41 @@
 # THIS ONLY WORKS IN GENERIC SETUPS AND MAY BREAK THINGS, POSSIBLY IRREVOKABLY, FOR
 #  UNUSAL SETUPS, KERNEL COMPILATIONS, DRIVERS, AND SO ON - USE AT OWN RISK!!!!
 
+if [ "$(whoami)" != 'root' ]; then
+	echo -e "\e[31;1m$0: ERROR: You have no permission to run $0 as non-root user.\e[0m"
+	exit 1;
+fi
 
 ## Handy guide
-# Debian 6 Squeeze
-# Debian 7 Wheezy
-# Debian 8 Jessie - "old old stable" about to expire as of this document
-# Debian 9 Stretch - old stable as of this document
-# Debian 10 Buster - current as of this document
-# Debian 11 Bullseye - testing as of this document
+# Debian 6 squeeze
+# Debian 7 wheezy
+# Debian 8 jessie - "old old stable" about to expire as of this document
+# Debian 9 stretch - old stable as of this document
+# Debian 10 buster - current as of this document
+# Debian 11 bullseye - testing as of this document
 
 # Swap which with which.  This seems to break down between 8 and 9, when 
 #  you have to change the conf from "archive" to the current ones
 
-    old_and_busted="squeeze"
-    new_hotness="wheezy"
+OLD_AND_BUSTED="squeeze"
+NEW_HOTNESS="wheezy"
 
 # Handy space savers
-    sudo apt-get clean
-    sudo apt-get autoremove -y
+apt-get clean
+apt-get autoremove -y
     
 # One last update
-    sudo apt-get update 
-    sudo apt-get upgrade -y --force-yes
+apt-get update 
+apt-get upgrade -y --force-yes
 
-    version=$(cat /etc/debian_version)
-    echo -e "\e[37;1mDEBIAN $version \e[0m\n"
+DEB_VERSION=$(cat /etc/debian_version)
+echo -e "\e[37;1mDEBIAN ${DEB_VERSION} \e[0m\n"
 
-    sudo sed -i 's/$old_and_busted/$new_hotness/g' /etc/apt/sources.list
+sed -i "s/${OLD_AND_BUSTED}/${NEW_HOTNESS}/g" /etc/apt/sources.list
 
-    sudo apt-get update
-    sudo apt-get upgrade -y --force-yes
-    sudo apt-get dist-upgrade -y --force-yes
+apt-get update
+apt-get upgrade -y --force-yes
+apt-get dist-upgrade -y --force-yes
 
-
-    echo -e "----------------\n :: NOW REBOOT :: \n----------------\n"
+echo -e "----------------\n :: NOW REBOOT :: \n----------------\n"
+exit 0;
